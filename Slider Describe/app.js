bb7e98052bad9f38f96b07aad10cfbba346655b1
scElChapter24 <156519828+scElChapter24@users.noreply.github.com>
2024-01-17 17:40:06 +0700

    Update app.js\

--- Slider Describe/app.js
+++ Slider Describe/app.js
@@ -1,7 +1,7 @@
 let nextButton = document.getElementById('next');
 let prevButton = document.getElementById('prev');
-let carousel = document.querySelector('.carousel');
-let listHTML = document.querySelector('.carousel .list');
+let describe = document.querySelector('.describe');
+let listHTML = document.querySelector('.describe .list');
 let seeMoreButtons = document.querySelectorAll('.seeMore');
 let backButton = document.getElementById('back');
 
@@ -16,14 +16,14 @@ const showSlider = (type) => {
     nextButton.style.pointerEvents = 'none';
     prevButton.style.pointerEvents = 'none';
 
-    carousel.classList.remove('next', 'prev');
-    let items = document.querySelectorAll('.carousel .list .item');
+    describe.classList.remove('next', 'prev');
+    let items = document.querySelectorAll('.describe .list .item');
     if(type === 'next'){
         listHTML.appendChild(items[0]);
-        carousel.classList.add('next');
+        describe.classList.add('next');
     }else{
         listHTML.prepend(items[items.length - 1]);
-        carousel.classList.add('prev');
+        describe.classList.add('prev');
     }
     clearTimeout(unAcceppClick);
     unAcceppClick = setTimeout(()=>{
@@ -33,10 +33,10 @@ const showSlider = (type) => {
 }
 seeMoreButtons.forEach((button) => {
     button.onclick = function(){
-        carousel.classList.remove('next', 'prev');
-        carousel.classList.add('showDetail');
+        describe.classList.remove('next', 'prev');
+        describe.classList.add('showDetail');
     }
 });
 backButton.onclick = function(){
-    carousel.classList.remove('showDetail');
+    describe.classList.remove('showDetail');
 }
